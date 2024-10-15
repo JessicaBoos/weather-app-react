17b3ec09ab3e33142a4949d4123437d4d21762ec
JessicaBoos <jessica.boos28@gmail.com>
2024-10-15 13:05:19 +0200

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by
+        <a href="https://github.com/JessicaBoos" target="_blank">
+          Jessica Boos
+        </a>
+        and is
+        <a
+          href="https://github.com/JessicaBoos/weather-app-react"
+          target="_blank"
+        >
+          open sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
