6eb681f475ef002743efdd84b13138cca2f5bfa0
JessicaBoos <jessica.boos28@gmail.com>
2024-10-16 12:47:23 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by
-        <a href="https://github.com/JessicaBoos" target="_blank">
-          Jessica Boos
-        </a>
-        and is
-        <a
-          href="https://github.com/JessicaBoos/weather-app-react"
-          target="_blank"
-        >
-          open sourced on GitHub.
-        </a>
-      </footer>
+      {" "}
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/JessicaBoos" target="_blank">
+            Jessica Boos
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/JessicaBoos/weather-app-react"
+            target="_blank"
+          >
+            open sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
