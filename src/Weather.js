c2269c335ab9d4b9943858d469446280fce7943c
JessicaBoos <jessica.boos28@gmail.com>
2024-10-17 12:32:05 +0200

    optimized useState

--- src/Weather.js
+++ src/Weather.js
@@ -3,14 +3,11 @@ import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
-    setTemperature(Math.round(response.data.temperature.current));
-    setReady(true);
     setWeatherData({
+      ready: true,
       city: response.data.city,
       description: response.data.condition.description,
       temperature: Math.round(response.data.temperature.current),
@@ -20,7 +17,7 @@ export default function Weather() {
     });
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <form>
@@ -39,22 +36,21 @@ export default function Weather() {
           </div>
         </form>
 
-        <h1>New York</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
           <li>Wednesday 07:00</li>
-          <li>Mostly Cloudy</li>
+          <li>{weatherData.description}</li>
         </ul>
         <div className="row">
           <div className="col-6 weather-icon d-flex">
             <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=" />
-            <span className="temperature">{temperature}</span>
+            <span className="temperature">{weatherData.temperature}</span>
             <span className="degrees">°C</span>
           </div>
           <div className="col-6">
             <ul>
-              <li>Percipitation : 15%</li>
-              <li>Humidity : 70%</li>
-              <li>Wind : 13km/h</li>
+              <li>Humidity : {weatherData.humidity}%</li>
+              <li>Wind : {weatherData.wind}km/h</li>
             </ul>
           </div>
         </div>
